fc6da067fb3e0a590aafbe7a52dc2e901ebd8df9
Elysia <c.elysia@foxmail.com>
2024-07-30 22:24:34 +0800

    Finished version analyzer

--- main.cpp
+++ main.cpp
@@ -1,26 +1,46 @@
 #include "pkm.h"
-
+#include <iostream>
 int main() {
-  PackageManager manager;
+  // PackageManager manager;
 
-  Package Dep1("Dep1", "1.0", {}, PackageStatus::UNINSTALLED);
-  Package Dep2("Dep2", "1.0", {}, PackageStatus::UNINSTALLED);
-  manager.addPackage(Dep1);
-  manager.addPackage(Dep2);
+  // Package Dep1("Dep1", "1.0", {}, PackageStatus::UNINSTALLED);
+  // Package Dep2("Dep2", "1.0", {}, PackageStatus::UNINSTALLED);
+  // manager.addPackage(Dep1);
+  // manager.addPackage(Dep2);
 
-  Package testPackage("TestApp", "1.0", {{"Dep1", "2.0.0"}, {"Dep2", "1.0.0"}}, PackageStatus::TOINSTALL);
+  // Package testPackage("TestApp", "1.0", {{"Dep1", "2.0.0"}, {"Dep2", "1.0.0"}}, PackageStatus::TOINSTALL);
 
-  auto pkgInstList = std::make_shared<std::vector<Package>>();
-  if (manager.checkDependencies(testPackage, pkgInstList)) {
-    std::cout << "Successfully resolved deps." << std::endl;
-    std::cout << "Need to install following package(s):" << std::endl;
-    for (auto const &p : *pkgInstList) {
-      std::cout << "  " << p.name << " " << p.version << std::endl;
-    }
+  // auto pkgInstList = std::make_shared<std::vector<Package>>();
+  // if (manager.checkDependencies(testPackage, pkgInstList)) {
+  //   std::cout << "Successfully resolved deps." << std::endl;
+  //   std::cout << "Need to install following package(s):" << std::endl;
+  //   for (auto const &p : *pkgInstList) {
+  //     std::cout << "  " << p.name << " " << p.version << std::endl;
+  //   }
 
-  } else {
-    std::cout << "Unable to resolve deps" << std::endl;
-  }
+  // } else {
+  //   std::cout << "Unable to resolve deps" << std::endl;
+  // }
 
-  return 0;
+  // auto result1 = comparePkgVersion("1.0.0", "1.0.0");
+  // auto result2 = comparePkgVersion("1.0.0", "1.0.1");
+  // auto result3 = comparePkgVersion("1.0.0", "1.1.0");
+  // auto result4 = comparePkgVersion("1.0.0", "2.0.0");
+
+  // // Test version numbers that contains special characters
+  // auto result5 = comparePkgVersion("1.0.0-alpha", "1.0.0-alpha");
+  // auto result6 = comparePkgVersion("1.0.0-alpha", "1.0.0-beta");
+  // auto result7 = comparePkgVersion("1.0.0-alpha", "1.0.0-alpha.1");
+  // // contain ~ . +
+  // auto result8 = comparePkgVersion("1.0.0+1", "1.0.0");
+  // auto result9 = comparePkgVersion("1.0.0~1", "1.0.0");
+  // auto result10 = comparePkgVersion("1.0.0-1", "1.0.0");
+  // auto result11 = comparePkgVersion("1.0.0-1", "1.0.0~1");
+
+  // // Test other version number
+  // auto result12 = comparePkgVersion("3", "3.14");
+  // auto result13 = comparePkgVersion("1.2.1", "a.1");
+  // auto result14 = comparePkgVersion("a.b.c", "a.b.d");
+  // auto result15 = comparePkgVersion("3-1", "3+1");
+  // return 0;
 }
--- pkm.h
+++ pkm.h
@@ -1,18 +1,13 @@
 #ifndef __PKM_H__
 #define __PKM_H__
 
-#include <iostream>
 #include <map>
+#include <memory>
 #include <string>
 #include <tuple>
 #include <vector>
-#include <memory>
 
-enum class PackageStatus {
-    UNINSTALLED = 0,
-    INSTALLED,
-    TOINSTALL
-};
+enum class PackageStatus { UNINSTALLED = 0, INSTALLED, TOINSTALL };
 
 struct Package {
   std::string name;
@@ -23,15 +18,48 @@ struct Package {
 
   Package(const Package &other);
 
-  inline Package(std::string name, std::string version, 
-                 std::vector<std::tuple<std::string, std::string>> dependencies, PackageStatus status = PackageStatus::UNINSTALLED)
-      : name(name), version(version), dependencies(dependencies), status(status) {}
+  inline Package(std::string name, std::string version,
+                 std::vector<std::tuple<std::string, std::string>> dependencies,
+                 PackageStatus status = PackageStatus::UNINSTALLED)
+      : name(name),
+        version(version),
+        dependencies(dependencies),
+        status(status) {}
 };
+
+enum class VersionCompareResult { EQUAL = 0, OLDER, NEWER, UNKNOWN };
+
+/**
+ * @brief VersionNumberPart
+ *
+ * Helper class to compare version number parts
+ */
+class VersionNumberPart {
+ public:
+  VersionNumberPart(std::string value);
+
+  std::string getValue() const;
+
+ private:
+  std::string value;
+
+ public:
+  bool operator<(const VersionNumberPart &other) const;
+
+  bool operator>(const VersionNumberPart &other) const;
+
+  bool operator==(const VersionNumberPart &other) const;
+};
+
+VersionCompareResult comparePkgVersion(const std::string &pkg1,
+                                       const std::string &pkg2);
 
 class PackageManager {
  public:
   void addPackage(const Package &pkg);
-  bool checkDependencies(const Package &pkg, std::shared_ptr<std::vector<Package>> pkgInstallList = nullptr);
+  bool checkDependencies(
+      const Package &pkg,
+      std::shared_ptr<std::vector<Package>> pkgInstallList = nullptr);
 
  private:
   std::map<std::string, Package> packages;
